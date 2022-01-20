30e1bb328558563fb3709a60a64da5d5ab171fc7
jayme <jaymeliao1226@gmail.com>
2022-01-19 22:01:53 -0500

    add bootstraps styles

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require("express")
 const bodyParser=require("body-parser")
 const app =express();
 
-
+app.use(express.static("public"));
 app.get("/", function(req,res){
     res.sendFile(__dirname+"/index.html");
 
